9407f732e00ffe02da3f70962178452065b99ca9
Fazilei Jett M. Mangulabnan <iamfazileijett@gmail.com>
2024-10-15 23:04:06 +0800

    Add files via upload

---
+++ general_content.php
@@ -0,0 +1,71 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet/general_content.css">
+    <title>Quizzes</title>
+</head>
+<body>
+    <div class="container">
+    <header class="header">
+    <button class="button">Upcoming</button>
+    <button class="button">Incomplete</button>
+    <button class="button">Completed</button>
+
+    <!-- Dropdown Filter Button -->
+    <div class="dropdown">
+        <button class="button dropdown-button"> <img src="media/filter.svg" alt="Filter Icon" class="filter-icon">Filter By</button>
+        <div class="dropdown-menu">
+            <button class="dropdown-item">Public</button>
+            <button class="dropdown-item">Private</button>
+        </div>
+    </div>
+</header>
+
+        <script src="scripts/general_content.js"></script>
+        <hr class="divider">
+
+        <div class="quiz-list">
+        <p class="date"> September 20th </p>
+            <div class="quiz-card">
+            <div class="quiz-wrapper">
+                <!-- <div class="image-container">
+                    <img src="media/history.jpg" alt="Quiz" class="quiz-image">
+                </div> -->
+                <div class="details-wrapper">
+                    <h2 class="quiz-title">History Quiz 1</h2>
+                    <hr>
+                    <div class="date-wrapper">
+                        <img src="media/calendar.svg" alt="Calendar Icon" class="image icon">
+                        <p class="due-date">Due Date: Sept 20 1:00 PM</p>
+                    </div>
+                    <p class="course-code">Section: BSH2002</p>
+                </div>
+            </div>
+            </div>
+            <p class="date"> September 21st </p>
+            <div class="quiz-card">
+            <div class="quiz-wrapper">
+                <!-- <div class="image-container">
+                    <img src="media/history.jpg" alt="Quiz" class="quiz-image">
+                </div> -->
+                <div class="details-wrapper">
+                    <h2 class="quiz-title">History Quiz 2</h2>
+                    <hr>
+                    <div class="date-wrapper">
+                        <img src="media/calendar.svg" alt="Calendar Icon" class="image icon">
+                        <p class="due-date">Due Date: Sept 21 1:00 PM</p>
+                    </div>
+                    <p class="course-code">Section: BSH2002</p>
+                </div>
+            </div>
+
+
+
+            
+
+</body>
+</html>
+ 
